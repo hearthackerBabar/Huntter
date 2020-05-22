022d69dc0770117360f5343a5ce2e0510454991a
Babar Ali <62939512+hearthackerBabar@users.noreply.github.com>
2020-05-23 00:10:42 +0500

    Update Linux.py

--- Linux.py
+++ Linux.py
@@ -166,7 +166,7 @@ def login1():
 		jalan(' \033[1;91mWarning: \033[1;92mTermux All Version Work ' )
 		jalan(' \033[1;91mWarning: \033[1;92mYour Internet Should Be Fast ' )
 		
-		print('\033[1;97m\x1b[1;96m<<---------Login Your Facebook Account--------->>\x1b[1;97m' )
+		print('\033[1;97m\x1b[1;92m---------Login Your Facebook Account---------\x1b[1;97m' )
 		print('	' )
 		id = raw_input('\033[1;97m[*] \x1b[1;91mFacebook/Email\x1b[1;93m: \x1b[1;93m')
 		pwd = raw_input('\033[1;97m[*] \x1b[1;91mPassword      \x1b[1;91m: \x1b[1;92m')
@@ -367,8 +367,9 @@ def pilih_super():
 	titik = ['.   ','..  ','... ']
 	for o in titik:
 		print("\r\033[1;93m•Cloning Has Been Started•\033[1;93m"+o),;sys.stdout.flush();time.sleep(0.05)
-	
-print "\033[1;95m<<-----------------\033[1;91mKalilinux\033[1;95m----------------->>"
+
+	print "\033[1;95m<<---------------\033[1;91mKalilinux\033[1;95m--------------->>"
+
 	print "\n\033[1;92mNOTE:\x1b[1;93m [If You Want to Stop Process Press CTRL+Z]"
 	
 			
@@ -586,13 +587,11 @@ print "\033[1;95m<<-----------------\033[1;91mKalilinux\033[1;95m---------------
 		
 	p = ThreadPool(30)
 	p.map(main, id)
-	print "\033[1;95m<<---------------\033[1;91mKalilinux\033[1;95m--------------->>""""
-
- #Dev:Babar_Ali
+#Dev:Babar_Ali
 	print '\033[1;91m•Process Has Been Completed•'
 	print"\033[1;94mTotal\033[1;92mOK/\x1b[1;91mCP \033[1;91m: \033[1;91m"+str(len(oks))+"\033[1;97m/\033[1;92m"+str(len(cekpoint))
 
-print"	\033[1;95m<<-----------------\033[1;91mKalilinux\033[1;95m----------------->>"""
+print"	\033[1;95m«-----------------\033[1;91mKalilinux\033[1;95m-----------------»"""
 	print """
              
 
@@ -605,7 +604,7 @@ print"	\033[1;95m<<-----------------\033[1;91mKalilinux\033[1;95m---------------
 \033[1;91m──────────────────╔═╝║
 \033[1;91m──────────────────╚══╝
 """
-	print "\033[1;95m<<-----------------\033[1;91mKalilinux\033[1;95m----------------->>"
+	print "\033[1;95m«-----------------\033[1;91mKalilinux\033[1;95m-----------------»"
 	raw_input("\n\033[1;97m[\033[1;95mBack\033[1;97m]")
 	menu()
 
