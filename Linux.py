8bb3515cb903aefd09adeacf138f46595e310746
Babar Ali <62939512+hearthackerBabar@users.noreply.github.com>
2020-06-28 22:47:07 +0500

    Update Linux.py

--- Linux.py
+++ Linux.py
@@ -125,13 +125,13 @@ def lisensi():
 def login():
 	os.system('clear')
 	print logo
-	print "\033[1;35m[1]\033[1;41m\033[1;34mLogin With Facebook\033[1;0m"
+	print "\033[1;97m[1]\033[1;47m\033[1;34mLogin With Facebook\033[1;0m"
         time.sleep(0.05)
-        print "\033[1;35m[2]\033[1;41m\033[1;34mLogin With Token\033[1;0m"
+        print "\033[1;97m[2]\033[1;47m\033[1;34mLogin With Token\033[1;0m"
         time.sleep(0.05)
-        print "\033[1;35m[3]\033[1;41m\033[1;34mDownload Token App\033[1;0m"
+        print "\033[1;97m[3]\033[1;47m\033[1;34mDownload Token App\033[1;0m"
         time.sleep(0.05)
-	print "\033[1;35m[0]\033[1;41m\033[1;34mExit\033[1;0m"
+	print "\033[1;97m[0] \033[1;34mExit"
 	pilih_login()
 
 def pilih_login():
