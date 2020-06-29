f8aefa2a6dfa10a70051e79a1254523cdd50c862
Babar Ali <62939512+hearthackerBabar@users.noreply.github.com>
2020-06-29 20:13:36 +0500

    Update Linux.py

--- Linux.py
+++ Linux.py
@@ -390,7 +390,7 @@ def pilih_super():
 		try:
 			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 			op = json.loads(jok.text)
-			print"\033[1;47m\033[1;93mName\033[1;93m:\033[1;97m "+op["name"]\033[1;0m
+			print"\033[1;47m\033[1;93mName\033[1;93m:\033[1;0m\033[1;97m "+op["name"]
 		except KeyError:
 			print"\x1b[1;92mID Not Found!"
 			raw_input("\n\033[1;96m[\033[1;47m\033[1;97mBack\033[1;96m]\033[1;0m")
