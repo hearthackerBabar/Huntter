f6265ca29a07187013779f29d9d60312bffe3893
Babar Ali <62939512+hearthackerBabar@users.noreply.github.com>
2020-09-01 07:08:32 +0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -109,7 +109,7 @@ listgrup = []
 vulnot = "\033[31mNot Vuln"
 vuln = "\033[32mVuln"
 
-kali linux.system("clear")
+linux.system("clear")
 print  """
 \033[1;97m#     #  #####        #    # #       #     #
 \033[1;97m#     # #     #       #   #  #        #   #
